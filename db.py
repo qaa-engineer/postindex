aeac014e82f76f1b2564002d3d1d76d28742e62e
trystep <none>
2020-04-05 19:56:19 +0300

    Сделан вывод данных с БД

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import psycopg2
+from postindex.classes import Postindex
 
 # connect to the db
 connection = psycopg2.connect(
@@ -7,15 +8,19 @@ connection = psycopg2.connect(
     user='postgres',
     password='djksa%s@w'
 )
+
 # cursor
 cursor = connection.cursor()
-SQL_query = "SELECT postindex, opsname, region, area, city FROM public.postindex"
+SQL_query = "SELECT * FROM public.postindex"
 cursor.execute(SQL_query)
 rows = cursor.fetchall()
+row=rows[207]
 
-print(rows[47000])
+content=Postindex(row)
+content.all_content()
 
-#close the cursor
+
+# close the cursor
 cursor.close()
 
 # close connection
