5d6430400daf4a594d901026159133b54d6cfeea
trystep <none>
2020-04-11 10:17:01 +0300

    Refactoring

--- project/app/views.py
+++ project/app/views.py
@@ -8,7 +8,30 @@ def index(request):
 def postindex(request):
     data=Postindex.objects.all()[5909:5910]
     for row in data:
-        post_type=get_post_type(row.post_type)
+        id = row.id
+        post_index_value = row.post_index_value
+        post_name =row.post_name
+        post_type = get_post_type(row.post_type)
+        post_sub = row.post_sub
+        region = row.region
+        autonomy = row.autonomy
+        area = row.area
+        city = row.city
+        city_1 = row.city_1
+        actual_date = row.actual_date
+        index_old = row.index_old
     context={'db':data,
-             'post_type':post_type}
+             'id':id,
+             'post__index_value':post_index_value,
+             'post_name': post_name,
+             'post_type': post_type,
+             'post_sub':post_sub,
+             'region':region,
+             'autonomy':autonomy,
+             'area':area,
+             'city':city,
+             'city_1':city_1,
+             'actual_date':actual_date,
+             'index_old':index_old
+             }
     return render(request, 'postindex.html', context)
