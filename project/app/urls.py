f7530c0f5be58f21ac8cf952e5cbefdb77536c07
trystep <none>
2020-04-11 23:50:56 +0300

    Доделал пагинацию

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,11 +2,9 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
-from .views import AppListView, AppDetailView
 
 urlpatterns = [
     path('', views.index),
     path('postindex/', include([path('', views.post_list),
-                                path('1', views.post_detail),
                                 ])),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -11,24 +11,27 @@ def index(request):
 
 def post_list(request):
     """
-    Вывод списка постов
+    Вывод постов
     """
     posts = Postindex.objects.all()
-    paginator = Paginator(posts, 1)
-    # Получение номера пагинации страницы
-    page_number=request.GET.get('page', 1)
+    # Количество постов на странице
+    number_of_records_per_page = 1
+    paginator = Paginator(posts, number_of_records_per_page)
+    # Номер странцы пагинации по умолчанию
+    number_of_records_deafult=2
+    page_number = request.GET.get('page', number_of_records_deafult)
     page = paginator.get_page(page_number)
-    page = page.object_list
-    context = {'page': page}
-    return render(request, 'posts.html', context=context)
-
-
-def post_detail(request):
-    """
-    Вывод поста
-    """
-    data = Postindex.objects.all()[3:4]
-    for row in data:
+    is_paginated = page.has_other_pages()
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+    post = posts[:page.number]
+    for row in post:
         id = row.id
         post_index_value = row.post_index_value
         post_name = row.post_name
@@ -41,7 +44,7 @@ def post_detail(request):
         city_1 = row.city_1
         actual_date = row.actual_date
         index_old = row.index_old
-    context = {'db': data,
+    context = {'db': post,
                'id': id,
                'post_index_value': post_index_value,
                'post_name': post_name,
@@ -53,14 +56,11 @@ def post_detail(request):
                'city': city,
                'city_1': city_1,
                'actual_date': actual_date,
-               'index_old': index_old
+               'index_old': index_old,
+               'page_object': page,
+               'is_paginated': is_paginated,
+               'prev_url': prev_url,
+               'next_url': next_url,
+               'post': post,
                }
-    return render(request, 'postindex.html', context)
-
-
-class AppListView(ListView):
-    model = Postindex
-
-
-class AppDetailView(DeleteView):
-    model = Postindex
+    return render(request, 'post-list.html', context=context)
